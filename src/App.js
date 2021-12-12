e84ef1e2f681a74434756785d046e1a856ba772b
unknown <giancc159@gmail.com>
2021-12-12 13:08:36 -0800

    EC-01 ADD Layout and added routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Switch, Routes, Route, Swit } from 'react-router-dom';
+import MasterLayout from '../src/assets/layouts/admin/MasterLayout'
+import Dashboard from './assets/components/admin/Dashboard';
+import Profile from './assets/components/admin/Profiles';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+
+        <Routes>
+          <Route path="/" element={<div>Default Page Content</div>} />
+
+          {/*<Route  path="/admin" name="Admin" render={(props) => <MasterLayout {...props} />}/>   */}
+
+
+          <Route path="/admin" element={<MasterLayout />} >
+
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/profile" element={<Profile />} />
+
+          </Route>
+
+
+
+
+
+        </Routes>
+
+      </Router>
+
     </div>
   );
 }
---
+++ src/assets/layouts/admin/MasterLayout.js
@@ -0,0 +1,55 @@
+import React from 'react';
+//import  '../../assets/admin/css/styles.css';
+//import '../../assets/admin/js/scripts';
+import { Navigate } from 'react-router-dom';
+
+import { BrowserRouter as Router, Switch, Routes, Route, Swit, Outlet } from 'react-router-dom';
+import '../../admin/js/scripts.js'
+import '../../admin/css/styles.css'
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+import routes from '../../../routes/routes'
+
+
+const MasterLayout = () => {
+
+    return (
+        <div className="sb-nav-fixed">
+            <Navbar />
+            <div id="layoutSidenav">
+                <div id="layoutSidenav_nav">
+
+                    <Sidebar />
+                </div>
+                <div id="layoutSidenav_content">
+
+                    <main >
+                        <Outlet />
+                        <Routes>
+
+                            {routes.filter(route => route.component)
+                                .map(({ path, component: Component }, idx) => (
+                                    <Route
+                                        key={idx}
+                                        path={path}
+                                        element={<Component />}
+                                    />
+                                ))}
+                            <Route
+                                path="/admin"
+                                element={<Navigate to="/admin/dashboard" />}
+                            />
+                        </Routes>
+
+                    </main>
+
+                    <Footer />
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default MasterLayout;
