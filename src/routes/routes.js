970f2a7fd15ddc536ee2098834c3b9f45d24ea98
unknown <giancc159@gmail.com>
2021-12-12 13:28:56 -0800

    EC-02 [ADD] Add LOGIN n Register n changes in routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,15 @@
 import Dashboard from '../assets/components/admin/Dashboard';
 import Profile from '../assets/components/admin/Profiles';
+import Login from '../assets/frontend/Auth/Login';
+import Register from '../assets/frontend/Auth/Register';
 
 
 const routes = [
 {path:'/admin',exact:true,name:'Admin'},
 {path:'/admin/dashboard',exact:true,name:'Dashboard',component: Dashboard},
 {path:'/admin/profile',exact:true,name:'Profile',component: Profile},
+{path:'/admin/login',exact:true,name:'Login',component: Login},
+{path:'/admin/register',exact:true,name:'Register',component: Register},
 
 ];
 
