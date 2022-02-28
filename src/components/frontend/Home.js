0cec3529e8da4d435eee91627490e637a2f60349
unknown <giancc159@gmail.com>
2022-02-28 07:36:59 -0800

    [ADD]Added lottie in React Ecomm

--- src/components/frontend/Home.js
+++ src/components/frontend/Home.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
+
 function Home() {
 
-    return (
-        <div>
-            <h1>
-                I am Homde</h1>
+    const container = useRef(null)
 
+    useEffect(() => {
+        lottie.loadAnimation({
+            container: container.current,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require('../../../src/assets/Lottie/92131-ecommerce.json')
+        })
+    }, [])
+
+    return (
+        <div className="App">
+            <div className="container" ref={container}></div>
+
+
+            <section className="section section-xl section-shaped pb-250">
+                <div className="shape shape-style-1 bg-gradient-info">
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                </div>
+                <div className="separator separator-bottom separator-skew">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        preserveAspectRatio="none"
+                        version="1.1"
+                        viewBox="0 0 2560 100"
+                        x="0"
+                        y="0"
+                    >
+                        <polygon
+                            className="fill-white"
+                            points="2560 0 2560 100 0 100"
+                        />
+                    </svg>
+                </div>
+            </section>
         </div>
     );
 
