6421d010f861fc5910ba4a4180d0a7e7898cade1
unknown <giancc159@gmail.com>
2022-04-16 16:24:51 -0700

    [FIX] Fix images and position

--- src/components/frontend/Footer.js
+++ src/components/frontend/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
 function Footer() {
 
     return (
@@ -19,26 +21,33 @@ function Footer() {
                 <div class="single-footer">
                     <h4>Menu Principal</h4>
                     <ul>
-                        <li><a href="/"><i class="fas fa-chevron-right"></i> Inicio</a></li>
+                    <li><Link to="/"><i class="fas fa-chevron-right"></i>Inicio</Link></li>
+                    <li><Link to="/Collections"><i class="fas fa-chevron-right"></i>Coleccion</Link></li>
+                    <li><Link to="/cart"><i class="fas fa-chevron-right"></i>Compra</Link></li>
+
+                        {/* <li><a href="/"><i class="fas fa-chevron-right"></i> Inicio</a></li> */}
                         {/* <li><a href="/about"><i class="fas fa-chevron-right"></i> Sobre Nosotros</a></li> */}
-                        <li><a href="/Collections"><i class="fas fa-chevron-right"></i> Coleccion</a></li>
-                        <li><a href="/cart"><i class="fas fa-chevron-right"></i> Compra</a></li>
+                        {/* <li><a href="/Collections"><i class="fas fa-chevron-right"></i> Coleccion</a></li>
+                        <li><a href="/cart"><i class="fas fa-chevron-right"></i> Compra</a></li> */}
                     </ul>
                 </div>
                 <div class="single-footer">
                     <h4>quick links</h4>
                     <ul>
-                        <li><a href="/privacy"><i class="fas fa-chevron-right"></i> Política de privacidad</a></li>
-                        <li><a href="/terms"><i class="fas fa-chevron-right"></i> Terminos & Condiciones</a></li>
+                    <li><Link to="/privacy"><i class="fas fa-chevron-right"></i>Política de privacidad</Link></li>
+                    <li><Link to="/terms"><i class="fas fa-chevron-right"></i>Terminos & Condiciones</Link></li>
+
+                        {/* <li><a href="/privacy"><i class="fas fa-chevron-right"></i> Política de privacidad</a></li> */}
+                        {/* <li><a href="/terms"><i class="fas fa-chevron-right"></i> Terminos & Condiciones</a></li> */}
                         {/* <li><a href="/disclaimer"><i class="fas fa-chevron-right"></i> disclaimer</a></li> */}
                     </ul>
                 </div>
                 <div class="single-footer">
                     <h4>contact us</h4>
-                    <ul>
-                        <li><a href=""><i class="fas fa-map-marker-alt"></i> Av. Ricardo Elías Aparicio 740, La Molina</a></li>
-                        <li><a href=""><i class="fas fa-mobile-alt"></i> +88 0123 456 789</a></li>
-                        <li><a href=""><i class="far fa-envelope"></i> gabrielquezada159@gmail.com</a></li>
+                    <ul className="informationContact">
+                        <li><i class="fas fa-map-marker-alt"></i> Av. Ricardo Elías Aparicio 740, La Molina</li>
+                        <li><i class="fas fa-mobile-alt"></i> +88 0123 456 789</li>
+                        <li><i class="far fa-envelope"></i> juliana1591994@gmail.com</li>
                     </ul>
                     <div class="footer-social">
                         <a href=""><i class="fab fa-facebook-f"></i></a>
